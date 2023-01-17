5a32cfd3ccce8bcf7c0bcdaaedc174f0ef7d5099
Kun Qian <qiankun1995@gmail.com>
2023-01-16 19:13:49 -1000

    feat: about page with strapi and card style

--- components/events/EventListCard.vue
+++ components/events/EventListCard.vue
@@ -1,49 +1,52 @@
 <template>
   <ui-card v-shadow="9" class="m-4 event-card">
-    <NuxtLink :to="`/events/${id}`">
-      <ui-card-media class="card-media-bg bg-black/75">
-        <img
-          v-if="coverPictureUrl"
-          alt="event card picture"
-          :src="`${strapiEndpoint}${coverPictureUrl}`"
-          class="object-contain w-full h-48"
-        />
-        <div v-else class="w-full h-48"></div>
-        <ui-card-media-content>
-          <div
-            class="headline absolute bottom-0 left-0 w-full bg-gradient-to-r from-cyan-500/95 to-blue-500/95 p-2"
-          >
-            <h1 :class="[$tt('headline5'), 'text-white']">
-              {{ eventName }}
-            </h1>
-          </div>
-          <CalendarIcon
-            class="absolute -top-8 right-2 scale-75"
-            v-if="!isRepeatedEvent"
-            :date="new Date(event?.attributes?.StartDateTime)"
+    <ui-card-content>
+      <NuxtLink :to="`/events/${id}`">
+        <ui-card-media class="card-media-bg bg-black/75">
+          <img
+            v-if="coverPictureUrl"
+            alt="event card picture"
+            :src="`${strapiEndpoint}${coverPictureUrl}`"
+            class="object-contain w-full h-48"
           />
-        </ui-card-media-content>
-      </ui-card-media>
-      <div class="p-2 text-white text-xs" :class="$tt('body2')">
-        <div>
-          <template v-if="isRepeatedEvent">Every {{ day }}</template>
-          <template v-else>{{ startDateTime }} - {{ endDateTime }}</template>
+          <div v-else class="w-full h-48"></div>
+          <ui-card-media-content>
+            <div
+              class="headline absolute bottom-0 left-0 w-full bg-gradient-to-r from-cyan-500/95 to-blue-500/95 p-2"
+            >
+              <h1 :class="[$tt('headline5'), 'text-white']">
+                {{ eventName }}
+              </h1>
+            </div>
+            <CalendarIcon
+              class="absolute -top-8 right-2 scale-75"
+              v-if="!isRepeatedEvent"
+              :date="new Date(event?.attributes?.StartDateTime)"
+            />
+          </ui-card-media-content>
+        </ui-card-media>
+
+        <div class="p-2 text-white text-xs" :class="$tt('body2')">
+          <div>
+            <template v-if="isRepeatedEvent">Every {{ day }}</template>
+            <template v-else>{{ startDateTime }} - {{ endDateTime }}</template>
+          </div>
+          <ui-list-divider class="mt-2 mb-2"></ui-list-divider>
+          <div class="flex items-center m-1" v-if="address">
+            <ui-icon size="18" class="mr-2">location_on</ui-icon>
+            {{ address }}
+          </div>
+          <div class="flex items-center m-1" v-if="phone">
+            <ui-icon size="18" class="mr-2">phone</ui-icon>
+            {{ phone }}
+          </div>
+          <div class="flex items-center m-1" v-if="pointOfContact">
+            <ui-icon size="18" class="mr-2">account_circle</ui-icon>
+            {{ pointOfContact }}
+          </div>
         </div>
-        <ui-list-divider class="mt-2 mb-2"></ui-list-divider>
-        <div class="flex items-center m-1" v-if="address">
-          <ui-icon size="18" class="mr-2">location_on</ui-icon>
-          {{ address }}
-        </div>
-        <div class="flex items-center m-1" v-if="phone">
-          <ui-icon size="18" class="mr-2">phone</ui-icon>
-          {{ phone }}
-        </div>
-        <div class="flex items-center m-1" v-if="pointOfContact">
-          <ui-icon size="18" class="mr-2">account_circle</ui-icon>
-          {{ pointOfContact }}
-        </div>
-      </div>
-    </NuxtLink>
+      </NuxtLink>
+    </ui-card-content>
   </ui-card>
 </template>
 
--- pages/About.vue
+++ pages/About.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>about</div>
+  <div class="about-container">
+    <div class="p-4">
+      <ui-card outlined>
+        <div class="p-4" v-html="content"></div>
+      </ui-card>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const graphql = useStrapiGraphQL();
+const data = ref({});
+
+data.value = await graphql(`
+  query getMeta {
+    about {
+      data {
+        attributes {
+          Content
+        }
+      }
+    }
+  }
+`);
+
+const content = computed(
+  () => data.value?.data?.about?.data?.attributes?.Content ?? ''
+);
+</script>
+
+<style lang="scss" scoped>
+.about-container::before {
+  @include flower-background;
+}
+</style>
